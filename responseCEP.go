e094b9dc8e31aa9e8412fbbffdb0f50275961e3a
Anderson Sutil <adsslipknot@gmail.com>
2025-07-04 15:46:34 -0300

    Challenge multiThreading with Jbrains June.

--- responseCEP.go
+++ responseCEP.go
@@ -1,15 +1,13 @@
 package main
 
-// responseCEP represents the combined response from either API
-// The struct includes fields from both Brasil API and ViaCEP API
 type responseCEP struct {
 	Cep         string `json:"cep"`
 	Logradouro  string `json:"logradouro"`
 	Complemento string `json:"complemento"`
 	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"` // cidade in Brasil API
-	UF          string `json:"uf"`         // estado in Brasil API
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
 	IBGE        string `json:"ibge"`
 	DDD         string `json:"ddd"`
-	Source      string // Indicates which API provided the response
+	Source      string
 }
